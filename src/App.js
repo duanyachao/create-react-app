f1fc16ec1de94e3d2169e8e092c298e3b676b6c3
duanyachao <xuanyingsuixing@163.com>
2020-06-23 09:59:20 +0800

    22222222222222222222
    Signed-off-by: duanyachao <xuanyingsuixing@163.com>

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ export default function App()  {
               <Link to="/about">About</Link>
             </li>
             <li>
-              <Link to="/dashboard">Bashboard</Link>
+              <Link to="/dashboard">Bashboard11111111</Link>
             </li>
           </ul>
         </div>
